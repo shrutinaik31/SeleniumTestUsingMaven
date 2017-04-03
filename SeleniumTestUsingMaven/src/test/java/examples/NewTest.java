74af3f1129107f01025df0d47f17d10fd59e0204
shrut <shruti.naik31@gmail.com>
2017-04-03 14:15:31 -0500

    ashdbansd

---
+++ SeleniumTestUsingMaven/src/test/java/examples/NewTest.java
@@ -0,0 +1,27 @@
+package examples;		
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;		
+public class NewTest {		
+	    private WebDriver driver;		
+		@Test				
+		public void testEasy() {	
+			driver.get("http://demo.guru99.com/selenium/guru99home/");  
+			String title = driver.getTitle();				 
+			Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+		}	
+		@BeforeTest
+		public void beforeTest() {	
+			System.setProperty("webdriver.chrome.driver", "D://Softwares//chromedriver.exe");
+			
+			driver = new ChromeDriver();  
+		}		
+		@AfterTest
+		public void afterTest() {
+			driver.quit();			
+		}		
+}	
